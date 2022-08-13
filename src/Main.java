64003dab883f80ec93a6d281867f1d530cff6a3a
Scherbakova <nastyasch0806@gmail.com>
2022-08-13 20:22:58 +0300

    hw-massiv1

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         //task1
-        int [] a = new int [] {1, 2, 3};
-        double [] b = {1.57, 7.654, 9.986};
-        byte [] c = {70, 13, 45,14};
+        int[] a = new int[]{1, 2, 3};
+        double[] b = {1.57, 7.654, 9.986};
+        byte[] c = {70, 13, 45, 14};
 
         //task2
 
-        for (int i = 0; i < 2; i++)
-        {
-            System.out.print(a[i] + "," );
+        for (int i = 0; i < 2; i++) {
+            System.out.print(a[i] + ",");
         }
-        int i1 = a [a.length - 1];
-            System.out.println(i1 );
+        int i1 = a[a.length - 1];
+        System.out.println(i1);
 
-        for (int d = 0; d < 2; d++)
-        {
+        for (int d = 0; d < 2; d++) {
             System.out.print(b[d] + ",");
 
         }
@@ -24,17 +22,37 @@ public class Main {
         System.out.println(d1);
 
 
-        for (int f = 0; f < 3; f++)
-        {
+        for (int f = 0; f < 3; f++) {
             System.out.print(c[f] + ",");
         }
-        int f1 = c [c.length - 1];
+        int f1 = c[c.length - 1];
         System.out.println(f1);
 
 
         //task3
 
 
+        for (int i2 = 2; i2 > 0; i2--)
+        {
+            System.out.print(a[i2] + ",");
+        }
+         int i2 = a[0];
+        System.out.println(i2);
+
+
+        for (int d2 = 2; d2 > 0; d2--)
+        {
+            System.out.print(b[d2] + ",");
+        }
+        double d2 = b[0];
+        System.out.println(d2);
+
+        for (int f2 = 3; f2 > 0; f2--)
+        {
+            System.out.print(c[f2] + ",");
+        }
+        int f2 = c[0];
+        System.out.println(f2);
 
     }
 }
