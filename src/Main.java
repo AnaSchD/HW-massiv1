23a710f130d6d9dc43c6575b9ea684d2c3ee0190
Scherbakova <nastyasch0806@gmail.com>
2022-08-13 19:52:04 +0300

    hw-massiv1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,37 @@ public class Main {
 
         //task1
         int [] a = new int [] {1, 2, 3};
+        double [] b = {1.57, 7.654, 9.986};
+        byte [] c = {70, 13, 45,14};
+
+        //task2
+
+        for (int i = 0; i < 2; i++)
+        {
+            System.out.print(a[i] + "," );
+        }
+        int i1 = a [a.length - 1];
+            System.out.println(i1 );
+
+        for (int d = 0; d < 2; d++)
+        {
+            System.out.print(b[d] + ",");
+
+        }
+        double d1 = b[b.length - 1];
+        System.out.println(d1);
+
+
+        for (int f = 0; f < 3; f++)
+        {
+            System.out.print(c[f] + ",");
+        }
+        int f1 = c [c.length - 1];
+        System.out.println(f1);
+
+
+        //task3
+
 
 
     }
